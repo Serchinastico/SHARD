8d50251bdd54d7091efa88e24c338277b138d4f0
Serchinastico <github@serchinastico.com>
2026-07-07 20:03:05 +0200

    Very simple test running microwakeword in mac

---
+++ packages/wake-word/run.py
@@ -0,0 +1,14 @@
+from pymicro_wakeword import MicroWakeWord, MicroWakeWordFeatures, Model
+import sounddevice as sd
+
+mww = MicroWakeWord.from_builtin(Model.ALEXA)
+mww_features = MicroWakeWordFeatures()
+
+stream = sd.InputStream(samplerate=16000, channels=1, dtype="int16")
+stream.start()
+
+while True:
+    audio, _ = stream.read(160)
+    for features in mww_features.process_streaming(audio.tobytes()):
+        if mww.process_streaming(features):
+            print("Detected!")
